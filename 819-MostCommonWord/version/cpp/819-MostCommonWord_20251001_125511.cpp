a96981c506c91cebcb08bd62b004ab42a23de630
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-01 12:55:14 +0530

    Create 819-MostCommonWord

---
+++ 819-MostCommonWord/version/cpp/819-MostCommonWord_20251001_125511.cpp
@@ -0,0 +1,44 @@
+// Last updated: 10/1/2025, 12:55:11 PM
+class Solution {
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+        //string stream 
+
+         for (char &c : paragraph) {
+            if (isalpha(c)) {
+                c = tolower(c);
+            } else {
+                c = ' '; 
+            }
+        }
+
+        stringstream ss(paragraph) ; 
+        string word ; 
+        unordered_map<string,int>mp ; 
+        set<string>st(banned.begin() , banned.end()) ; 
+
+        while(ss>>word)
+        {
+            mp[word]++ ; 
+
+        }
+        int maxcount = 0 ; 
+        string ans = "" ; 
+        for(auto &ele:mp)
+        {
+            if(st.find(ele.first)==st.end())
+            {
+                if(ele.second>maxcount)
+                {
+                    maxcount = ele.second ; 
+                    ans = ele.first ; 
+                }
+
+            }
+        }
+        return ans ; 
+        
+
+        
+    }
+};
