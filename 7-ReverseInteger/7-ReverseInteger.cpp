89893ab341e7d45e8a45ff1db289c759af374c4e
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 09:03:00 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -0,0 +1,13 @@
+// Last updated: 10/5/2025, 9:02:58 AM
+class Solution {                      
+public:
+    int reverse(int x) {
+        int r=0;      // decleare r 
+        while(x){
+         if (r>INT_MAX/10 || r<INT_MIN/10) return 0; // check 32 bit range if r is outside the range then return 0 
+         r=r*10+x%10; // find remainder and add its to r
+         x=x/10;     // Update the value of x
+        } 
+        return r;  // if r in the 32 bit range then return r
+    }
+}; 
