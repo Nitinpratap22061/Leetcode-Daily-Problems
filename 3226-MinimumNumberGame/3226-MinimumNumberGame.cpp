b2771a08d5e87dd2c707af28090fde01003bec5b
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:50:40 +0530

    Create 3226-MinimumNumberGame

---
+++ 3226-MinimumNumberGame/3226-MinimumNumberGame.cpp
@@ -0,0 +1,14 @@
+// Last updated: 10/5/2025, 8:50:38 AM
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i=i+2){
+            swap(nums[i],nums[i+1]);
+        }
+        return nums;
+            
+        
+        
+    }
+};
