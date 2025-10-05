8932fb3998a9cf23df693be6c6542ef63eb1d636
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:48:21 +0530

    Create 3973-FlipSquareSubmatrixVertically

---
+++ 3973-FlipSquareSubmatrixVertically/3973-FlipSquareSubmatrixVertically.cpp
@@ -0,0 +1,17 @@
+// Last updated: 10/5/2025, 8:48:19 AM
+class Solution {
+public:
+    vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
+        int i = x;
+        int j = x + k - 1;
+
+        while(i < j) {
+            for(int z = y; z < y + k; z++) {
+                swap(grid[i][z], grid[j][z]);
+            }
+            i++;
+            j--;
+        }
+        return grid;
+    }
+};
