2e8adbfd058ef3df8a0a05cb21c7fe974a9e0fd6
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 09:02:41 +0530

    Create 25-ReverseNodesInKGroup

---
+++ 25-ReverseNodesInKGroup/25-ReverseNodesInKGroup.cpp
@@ -0,0 +1,53 @@
+// Last updated: 10/5/2025, 9:02:40 AM
+class Solution {
+public:
+    ListNode* find(ListNode* node, int k) {
+        while (node && --k) {
+            node = node->next;
+        }
+        return node;
+    }
+
+    ListNode* rev(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr) {
+            ListNode* nextnode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextnode;
+        }
+        return prev;
+    }
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head || k == 1) return head;
+
+        ListNode* temp = head;
+        ListNode* prev = nullptr;
+
+        while (temp) {
+            ListNode* kthNode = find(temp, k);
+            if (!kthNode) {
+                if (prev) prev->next = temp;
+                break;
+            }
+
+            ListNode* nextnode = kthNode->next;
+            kthNode->next = nullptr;
+
+            ListNode* newHead = rev(temp);
+
+            if (temp == head) {
+                head = newHead;
+            } else {
+                prev->next = newHead;
+            }
+
+            prev = temp;
+            temp = nextnode;
+        }
+
+        return head;
+    }
+};
