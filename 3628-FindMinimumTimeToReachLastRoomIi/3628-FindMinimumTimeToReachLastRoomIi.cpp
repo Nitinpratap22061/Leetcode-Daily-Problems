2b1d4a59d3ec74bf16ab1ad96e7ecfe258d63df0
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:49:34 +0530

    Create 3628-FindMinimumTimeToReachLastRoomIi

---
+++ 3628-FindMinimumTimeToReachLastRoomIi/3628-FindMinimumTimeToReachLastRoomIi.cpp
@@ -0,0 +1,58 @@
+// Last updated: 10/5/2025, 8:49:33 AM
+class Solution {
+public:
+int m,n;
+     bool isSafe(int i , int j)
+     {
+        return (i>=0 && i<m && j>=0 && j<n);
+        
+            
+     }
+     vector<vector<int>>directions = { {1,0} , {0,1} , {-1,0} , {0,-1} };
+     typedef pair<int , pair<int,int>>P;
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+         m = moveTime.size() ; 
+         n = moveTime[0].size();
+        priority_queue<P , vector<P> ,  greater<P>>pq;
+        vector<vector<int>>result(m,vector<int>(n,INT_MAX));
+        pq.push({0 , {0,0}}); 
+        result[0][0] = 0 ;
+        int count  = 0 ;
+        //khandani dijkstra
+        while(!pq.empty())
+        {
+            int currTime = pq.top().first ; 
+            int i  = pq.top().second.first ; 
+            int j = pq.top().second.second;
+            pq.pop();
+            for(auto &dir : directions)
+            {
+                int new_i = i+dir[0];
+                int new_j = j+dir[1] ; 
+                if(isSafe(new_i , new_j))
+                {
+                    int wait = max(moveTime[new_i][new_j] - currTime , 0 );
+                    int finaltime ; 
+                    int steps = new_i+new_j;
+                    if(steps%2!=0)
+                    {
+                        finaltime = wait + currTime+1;
+
+                    }
+                    else{
+                         finaltime = wait + currTime+2;
+                    }
+                    
+                    if(result[new_i][new_j]>finaltime)
+                    {
+                        result[new_i][new_j] = finaltime;
+                        pq.push({finaltime , {new_i , new_j}});
+                    }
+                    
+                }
+            }
+        }
+        return result[m-1][n-1]; 
+        
+    }
+};
