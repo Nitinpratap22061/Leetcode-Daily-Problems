0d584f38b6fc953e14f2998a36b4440fe39f9f72
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:57:54 +0530

    Create 753-OpenTheLock

---
+++ 753-OpenTheLock/753-OpenTheLock.cpp
@@ -0,0 +1,58 @@
+// Last updated: 10/5/2025, 8:57:53 AM
+class Solution {
+public:
+    void  fillNeighbours(queue<string> &q , string &curr , unordered_set<string>&st )
+    {
+        for(int i = 0 ; i<4 ; i++)
+        {
+            char ch = curr[i];
+            char inc = ch=='9'?'0':ch+1;
+            char dec = ch=='0'?'9':ch-1;
+
+            curr[i]=dec ; 
+            if(st.find(curr)==st.end())
+            {
+                st.insert(curr);
+                q.push(curr);
+            }
+            curr[i] = inc;
+            if(st.find(curr)==st.end())
+            {
+                st.insert(curr);
+                q.push(curr);
+            }
+            curr[i] = ch;
+
+        }
+    }
+    int openLock(vector<string>& deadends, string target) {
+    unordered_set<string>st(begin(deadends) , end(deadends));
+    queue<string>q;
+    string start = "0000";
+    if(st.find(start)!=st.end())
+    {
+        return -1;
+    }
+    q.push(start);
+    int level = 0 ;
+    while(!q.empty())
+    {
+        int n = q.size();
+        while(n--)
+        {
+            string curr = q.front();
+            q.pop();
+            if(curr==target)
+            {
+                return level;
+            }
+            fillNeighbours(q , curr , st);
+        }
+        level++;
+    }
+    return -1;
+     
+
+        
+    }
+};
