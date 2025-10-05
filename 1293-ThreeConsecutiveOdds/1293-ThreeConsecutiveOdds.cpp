f34741936570d17c81b3c1980e28019652d21da4
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:56:05 +0530

    Create 1293-ThreeConsecutiveOdds

---
+++ 1293-ThreeConsecutiveOdds/1293-ThreeConsecutiveOdds.cpp
@@ -0,0 +1,19 @@
+// Last updated: 10/5/2025, 8:56:03 AM
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        if(arr.size()<3)
+        {
+            return false;
+        }
+        for(int i = 0 ; i<arr.size()-2 ; i++)
+        {
+            if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0)
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
