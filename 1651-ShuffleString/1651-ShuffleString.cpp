1b94897c37d0d3636bb5bb57ed84c253e315ddb9
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:54:58 +0530

    Create 1651-ShuffleString

---
+++ 1651-ShuffleString/1651-ShuffleString.cpp
@@ -0,0 +1,22 @@
+// Last updated: 10/5/2025, 8:54:56 AM
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string ans="";
+        int n=s.size();
+        char arr[n];
+        int j=0;
+        for(int i=0;i<indices.size();i++)
+        {
+            int idx=indices[i];
+            arr[idx]=s[j];
+            j++;
+        }
+        for(int i=0;i<n;i++)
+        {
+          ans+=arr[i];
+        }
+        return ans;
+        
+    }
+};
