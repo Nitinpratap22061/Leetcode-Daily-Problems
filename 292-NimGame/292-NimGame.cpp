cdceab0aa51352da4bd165ba1e3656f311e064d6
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:59:48 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.cpp
@@ -0,0 +1,13 @@
+// Last updated: 10/5/2025, 8:59:46 AM
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n%4==0)
+        {
+            return false;
+        }
+        return true;
+       
+        
+    }
+};
