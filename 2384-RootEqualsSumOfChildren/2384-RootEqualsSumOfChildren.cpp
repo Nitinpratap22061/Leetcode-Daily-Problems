e922d43070cd21979dc5864da115f106e0d15c89
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:52:35 +0530

    Create 2384-RootEqualsSumOfChildren

---
+++ 2384-RootEqualsSumOfChildren/2384-RootEqualsSumOfChildren.cpp
@@ -0,0 +1,23 @@
+// Last updated: 10/5/2025, 8:52:34 AM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        if(root->val == root->left->val + root->right->val)
+        {
+            return true;
+        }
+        return false;
+        
+    }
+};
