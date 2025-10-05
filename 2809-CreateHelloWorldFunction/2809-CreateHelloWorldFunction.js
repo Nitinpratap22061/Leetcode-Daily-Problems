35fc72855099d892268d2c63d48f8c47c3cce3b8
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:51:19 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,18 @@
+// Last updated: 10/5/2025, 8:51:17 AM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+  
+    
+    return function(...args) {
+
+        return "Hello World"
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
