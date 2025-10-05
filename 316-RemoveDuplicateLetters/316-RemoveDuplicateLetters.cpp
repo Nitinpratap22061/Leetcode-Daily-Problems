40ea1b51670530d7ae1d8e7b48b8e4d9ee6fa422
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:59:43 +0530

    Create 316-RemoveDuplicateLetters

---
+++ 316-RemoveDuplicateLetters/316-RemoveDuplicateLetters.cpp
@@ -0,0 +1,37 @@
+// Last updated: 10/5/2025, 8:59:41 AM
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+
+        int n = s.length() ; 
+        string res;
+
+        vector<bool>seen(26,false) ; 
+        vector<int>lastidx(26) ; 
+
+        for(int i = 0 ; i<n ; i++)
+        {
+            char ch = s[i] ; 
+            lastidx[ch-'a'] = i ;
+        }
+
+        for(int i = 0 ; i<n ; i++)
+        {
+            char ch = s[i] ; 
+            int idx = ch-'a' ; 
+
+            if(seen[idx] ==  true) continue ; 
+
+            while(!res.empty() && res.back()>ch && lastidx[res.back()-'a']>i)
+            {
+                seen[res.back()-'a'] = false ; 
+                res.pop_back() ; 
+
+            }
+            res.push_back(ch) ; 
+            seen[idx] = true ; 
+        }
+        return res;
+        
+    }
+};
