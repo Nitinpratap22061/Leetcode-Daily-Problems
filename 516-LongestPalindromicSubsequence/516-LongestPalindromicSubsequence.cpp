d6d823ee1729216552f3ef7a01deb1146607065f
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:58:51 +0530

    Create 516-LongestPalindromicSubsequence

---
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.cpp
@@ -0,0 +1,30 @@
+// Last updated: 10/5/2025, 8:58:49 AM
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+        vector<vector<int>>t(n,vector<int>(n));
+
+        for(int i = 0 ; i<n ; i++)
+        {
+            t[i][i] = 1 ;
+        }
+        for(int l=2 ; l<=n ; l++)
+        {
+            for(int i = 0 ; i<n-l+1 ; i++)
+            {
+                int j = i+l-1;
+                if(s[i] == s[j])
+                {
+                    t[i][j] = 2+t[i+1][j-1];
+                }
+                else
+                {
+                    t[i][j] = max(t[i+1][j] , t[i][j-1]);
+                }
+            }
+        }
+
+        return t[0][n-1];
+    }
+};
