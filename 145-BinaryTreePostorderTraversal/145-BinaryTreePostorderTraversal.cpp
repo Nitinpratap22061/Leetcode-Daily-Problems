7f9f92cd8afc458965e36bd9ca70a5e861e232b6
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 09:00:54 +0530

    Create 145-BinaryTreePostorderTraversal

---
+++ 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.cpp
@@ -0,0 +1,22 @@
+// Last updated: 10/5/2025, 9:00:52 AM
+#include <vector>
+
+
+class Solution {
+public:
+    void postorder(TreeNode* root, vector<int>& result) {
+        if (root == nullptr) {
+            return;
+        }
+
+        postorder(root->left, result);
+        postorder(root->right, result);
+        result.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        postorder(root, result);
+        return result;
+    }
+};
