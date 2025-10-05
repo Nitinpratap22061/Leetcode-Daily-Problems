701a34077026a1733d64ff32a852bc19be501427
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:55:19 +0530

    Create 1538-MaximumPointsYouCanObtainFromCards

---
+++ 1538-MaximumPointsYouCanObtainFromCards/1538-MaximumPointsYouCanObtainFromCards.cpp
@@ -0,0 +1,25 @@
+// Last updated: 10/5/2025, 8:55:17 AM
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+       int n = cardPoints.size() ; 
+        int lsum = 0;
+        int rsum = 0 ; 
+
+        for (int i = 0; i < k; ++i) {
+            lsum += cardPoints[i];
+        }
+
+        int maxScore = lsum;
+        int ridx = n-1 ; 
+        
+        for (int i = k-1; i >= 0; i--) {
+            lsum -= cardPoints[i];         
+            rsum += cardPoints[ridx]; 
+            ridx--;      
+            maxScore = max(maxScore, lsum+rsum);
+        }
+
+        return maxScore;
+    }
+};
