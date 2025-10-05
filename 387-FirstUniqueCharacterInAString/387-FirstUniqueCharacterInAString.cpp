4eeef079bdfa47507c71fea981e7b7a31d38fad9
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:59:22 +0530

    Create 387-FirstUniqueCharacterInAString

---
+++ 387-FirstUniqueCharacterInAString/387-FirstUniqueCharacterInAString.cpp
@@ -0,0 +1,17 @@
+// Last updated: 10/5/2025, 8:59:20 AM
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int>freq(26);
+        for(int i=0;i<s.size();i++)
+        {
+            freq[s[i]-'a']++;
+        }
+        for(int i=0;i<s.size();i++)
+        {
+               if(freq[s[i]-'a']==1)
+               return i;
+        }
+        return -1;
+    }
+};
