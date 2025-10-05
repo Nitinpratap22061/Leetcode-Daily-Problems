5495402e8d4a73a91c864401ed3c1b6661c3581c
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:51:23 +0530

    Create 2792-NeighboringBitwiseXor

---
+++ 2792-NeighboringBitwiseXor/2792-NeighboringBitwiseXor.cpp
@@ -0,0 +1,30 @@
+// Last updated: 10/5/2025, 8:51:22 AM
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+
+        int n = derived.size() ; 
+        vector<int>original(n);
+        original[0]=1 ; 
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            original[i+1] = original[i]^derived[i] ; 
+        }
+        if(original[n-1]^original[0] == derived[n-1])
+        {
+            return true;
+        }
+
+        original[0] = 0 ;
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            original[i+1] = original[i]^derived[i] ; 
+        }
+        if(original[n-1]^original[0] == derived[n-1])
+        {
+            return true;
+        }
+        return false;
+        
+    }
+};
