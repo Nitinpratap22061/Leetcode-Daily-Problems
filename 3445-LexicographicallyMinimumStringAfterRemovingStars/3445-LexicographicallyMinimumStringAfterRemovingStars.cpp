d247d6444abee30a2f4af903e3d77015f1b7a164
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:50:08 +0530

    Create 3445-LexicographicallyMinimumStringAfterRemovingStars

---
+++ 3445-LexicographicallyMinimumStringAfterRemovingStars/3445-LexicographicallyMinimumStringAfterRemovingStars.cpp
@@ -0,0 +1,45 @@
+// Last updated: 10/5/2025, 8:50:06 AM
+class Solution {
+public:
+    typedef pair<char,int>P ; 
+    class comp{
+        public:
+        bool operator()(P p1 , P p2)
+        {
+            if(p1.first==p2.first)
+            {
+                return p1.second<p2.second;
+            }
+            return p1.first>p2.first ; 
+        }
+    };
+    string clearStars(string s) {
+        int n = s.size();
+        priority_queue<P , vector<P> , comp>pq;
+        for(int i = 0 ; i<n ; i++)
+        {
+            if(s[i]!='*')
+            {
+                pq.push({s[i],i});
+
+            }
+            else
+            {
+                int index = pq.top().second;
+                pq.pop();
+                s[index] = '*' ;
+            }
+        }
+        string res = "";
+        for(int i = 0 ; i<n ; i++)
+        {
+            if(s[i]!='*')
+            {
+                res.push_back(s[i]);
+            }
+        }
+        return res;
+        
+        
+    }
+};
