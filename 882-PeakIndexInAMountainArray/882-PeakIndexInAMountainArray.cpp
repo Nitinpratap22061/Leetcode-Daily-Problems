fa2f922a189ab851c619c8b6d63e60e7698759ed
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-05 08:57:31 +0530

    Create 882-PeakIndexInAMountainArray

---
+++ 882-PeakIndexInAMountainArray/882-PeakIndexInAMountainArray.cpp
@@ -0,0 +1,22 @@
+// Last updated: 10/5/2025, 8:57:29 AM
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int left=0;
+        int right=arr.size()-1;
+        int mid;
+        while(left<right){
+            mid=left+(right-left)/2;
+            if(arr[mid]<arr[mid+1]){
+                left=mid+1;
+            }
+            else{
+                right=mid;
+            }
+            
+        }
+        return left;
+        
+        
+    }
+};
