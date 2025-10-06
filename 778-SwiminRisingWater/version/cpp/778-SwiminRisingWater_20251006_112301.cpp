719e12981ebf53ee479d6b5f80b3f16033feaf5a
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-06 11:23:04 +0530

    Create 778-SwiminRisingWater

---
+++ 778-SwiminRisingWater/version/cpp/778-SwiminRisingWater_20251006_112301.cpp
@@ -0,0 +1,31 @@
+// Last updated: 10/6/2025, 11:23:01 AM
+class Solution {
+public:
+    vector<vector<int>> directions = {{0,1},{1,0},{-1,0},{0,-1}};
+    
+    int swimInWater(vector<vector<int>>& grid) {
+        int m = grid.size();
+        vector<vector<int>> vis(m, vector<int>(m, 0));
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+
+        pq.push({grid[0][0], 0, 0});
+        vis[0][0] = 1;
+
+        while (!pq.empty()) {
+            auto t = pq.top(); pq.pop();
+            int time = t[0], i = t[1], j = t[2];
+
+            if (i == m-1 && j == m-1) return time;
+
+            for (auto &d : directions) {
+                int ni = i + d[0], nj = j + d[1];
+                if (ni >= 0 && nj >= 0 && ni < m && nj < m && !vis[ni][nj]) {
+                    vis[ni][nj] = 1;
+                    pq.push({max(time, grid[ni][nj]), ni, nj});
+                }
+            }
+        }
+
+        return -1;
+    }
+};
