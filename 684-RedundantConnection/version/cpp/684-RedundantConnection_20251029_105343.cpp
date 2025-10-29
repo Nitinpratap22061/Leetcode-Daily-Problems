00b1acfe4e73f0688d2da528123529e65e83df19
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-29 10:53:49 +0530

    Create 684-RedundantConnection

---
+++ 684-RedundantConnection/version/cpp/684-RedundantConnection_20251029_105343.cpp
@@ -0,0 +1,63 @@
+// Last updated: 10/29/2025, 10:53:43 AM
+class Solution {
+public:
+    class DSU {
+    public:
+        vector<int> parent;
+        vector<int> rank;
+
+        DSU(int n) {
+            parent.resize(n + 1);
+            rank.resize(n + 1);
+            for (int i = 0; i <= n; i++) {
+                parent[i] = i;
+                rank[i] = 0;
+            }
+        }
+
+        int find(int x) {
+            if (x == parent[x]) {
+                return parent[x];
+            }
+            return parent[x] = find(parent[x]); // path compression
+        }
+
+        void Union(int x, int y) {
+            int x_parent = find(x);
+            int y_parent = find(y);
+
+            if (x_parent == y_parent) {
+                return;
+            }
+
+            if (rank[x_parent] > rank[y_parent]) {
+                parent[y_parent] = x_parent;
+            } 
+            else if (rank[x_parent] < rank[y_parent]) {
+                parent[x_parent] = y_parent;
+            } 
+            else {
+                parent[x_parent] = y_parent;
+                rank[y_parent]++;
+            }
+        }
+    };
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        DSU dsu(n);
+
+        for (auto &edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            if (dsu.find(u) == dsu.find(v)) {
+                return edge;
+            }
+
+            dsu.Union(u, v);
+        }
+
+        return {};
+    }
+};
