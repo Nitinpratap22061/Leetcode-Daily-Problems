dbfa274d3c743734b0c8e1a4cbabac76b0cd0d6f
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-29 10:24:57 +0530

    Create 684-RedundantConnection

---
+++ 684-RedundantConnection/version/cpp/684-RedundantConnection_20251029_102452.cpp
@@ -0,0 +1,48 @@
+// Last updated: 10/29/2025, 10:24:52 AM
+class Solution {
+public:
+
+    bool dfs(unordered_map<int,vector<int>>& adj , int u , int v , vector<bool>& visited )
+    {
+        visited[u] = true ; 
+
+        if(u==v)
+        {
+            return true ; 
+        }
+        for(int &ngbr : adj[u])
+        {
+            if(!visited[ngbr])
+            {
+                if(dfs(adj , ngbr ,v , visited))
+                {
+                    return true ; 
+                }
+            }
+        }
+        return false ; 
+
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+
+        int n = edges.size() ; 
+
+        unordered_map<int,vector<int>>adj ; 
+
+        for(int i = 0 ; i<n ; i++)
+        {
+            int u = edges[i][0] ; 
+            int v = edges[i][1] ; 
+            vector<bool>visited(n,false) ; 
+
+            if(adj.find(u)!=adj.end() && adj.find(v)!=adj.end() && dfs(adj , u , v , visited))
+            {
+                return edges[i] ; 
+            }
+            adj[u].push_back(v) ; 
+            adj[v].push_back(u) ; 
+        }
+        return {} ; 
+        
+    }
+};
