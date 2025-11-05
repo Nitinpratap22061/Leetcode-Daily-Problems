b5a4d04fa73574ea2d0266ad7c918f4164d78c52
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-11-05 13:30:21 +0530

    Create 416-PartitionEqualSubsetSum

---
+++ 416-PartitionEqualSubsetSum/version/cpp/416-PartitionEqualSubsetSum_20251105_133021.cpp
@@ -0,0 +1,39 @@
+// Last updated: 11/5/2025, 1:30:21 PM
+class Solution {
+public:
+     int t[201][20001];
+    bool solve(vector<int>& nums , int i , int x)
+    {
+        if(x==0)
+        {
+            return true;
+        }
+        if(i>=nums.size())
+        {
+            return false;
+        }
+        if(t[i][x]!=-1)
+        {
+            return t[i][x];
+        }
+        bool take = false ; 
+        if(x>=nums[i])
+        {
+            take = solve(nums , i+1 , x-nums[i]);
+        }
+        bool skip = solve(nums,i+1,x);
+        return t[i][x] = take||skip;
+    }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size() ;
+        int s = accumulate(begin(nums) , end(nums) , 0);
+        if(s%2!=0)
+        {
+            return false;
+        }
+        int x  = s/2 ;
+        memset(t,-1,sizeof(t));
+        return solve(nums , 0 , x);
+        
+    }
+};
