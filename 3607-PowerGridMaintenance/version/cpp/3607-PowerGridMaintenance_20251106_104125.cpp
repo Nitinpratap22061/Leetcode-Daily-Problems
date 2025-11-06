b0dd725a4b8774a89e6e976c9250b8b8a9ae9b2c
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-11-06 10:41:25 +0530

    Create 3607-PowerGridMaintenance

---
+++ 3607-PowerGridMaintenance/version/cpp/3607-PowerGridMaintenance_20251106_104125.cpp
@@ -0,0 +1,74 @@
+// Last updated: 11/6/2025, 10:41:25 AM
+class Solution {
+public:
+    vector<int> parent;
+    vector<int> findParent;
+    vector<set<int>> onlineInComponent;
+
+    int find(int x) {
+        if (findParent[x] != x) {
+            findParent[x] = find(findParent[x]);
+        }
+        return findParent[x];
+    }
+
+    void unite(int u, int v) {
+        int pu = find(u);
+        int pv = find(v);
+        if (pu != pv) {
+            // Union smaller set into larger one to reduce time
+            if (onlineInComponent[pu].size() < onlineInComponent[pv].size()) {
+                swap(pu, pv);
+            }
+            findParent[pv] = pu;
+            onlineInComponent[pu].insert(onlineInComponent[pv].begin(), onlineInComponent[pv].end());
+            onlineInComponent[pv].clear();
+        }
+    }
+
+    vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
+        parent.resize(c + 1);
+        findParent.resize(c + 1);
+        onlineInComponent.resize(c + 1);
+
+        // Init DSU and sets
+        for (int i = 1; i <= c; ++i) {
+            findParent[i] = i;
+            onlineInComponent[i].insert(i);
+        }
+
+        // Union connections
+        for (auto& conn : connections) {
+            unite(conn[0], conn[1]);
+        }
+
+        vector<bool> isOnline(c + 1, true);
+        vector<int> result;
+
+        for (auto& query : queries) {
+            int type = query[0];
+            int x = query[1];
+
+            if (type == 1) {
+                if (isOnline[x]) {
+                    result.push_back(x);
+                } else {
+                    int root = find(x);
+                    if (onlineInComponent[root].empty()) {
+                        result.push_back(-1);
+                    } else {
+                        result.push_back(*onlineInComponent[root].begin());  // smallest online station
+                    }
+                }
+            } else {
+                if (isOnline[x]) {
+                    isOnline[x] = false;
+                    int root = find(x);
+                    onlineInComponent[root].erase(x);
+                }
+            }
+        }
+
+        return result;
+    }
+};
