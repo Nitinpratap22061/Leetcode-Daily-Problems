fb1edac70e5d307e16f13f1c900c638cb3b84f29
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-11-22 14:31:26 +0530

    Create

---
+++ problem-1763802082958/version/cpp/problem-1763802082958_20251122_143122.cpp
@@ -0,0 +1,20 @@
+// Last updated: 11/22/2025, 2:31:22 PM
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+
+        int n = nums.size() ; 
+        int count = 0 ; 
+
+        for(int i = 0 ; i<n ; i++)
+        {
+            if(nums[i]%3!=0)
+            {
+                count++ ; 
+            }
+        }
+
+        return count ; 
+        
+    }
+};
