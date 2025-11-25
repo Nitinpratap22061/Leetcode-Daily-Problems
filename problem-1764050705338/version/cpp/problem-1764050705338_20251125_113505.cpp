41e13e24befca807e174c494e1c73aaaac5a6cd2
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-11-25 11:35:06 +0530

    Create

---
+++ problem-1764050705338/version/cpp/problem-1764050705338_20251125_113505.cpp
@@ -0,0 +1,27 @@
+// Last updated: 11/25/2025, 11:35:05 AM
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        
+        if(k % 2 == 0 || k % 5 == 0)
+        {
+              return -1;
+        } 
+
+        long long num = 1;
+        int len = 1;
+
+        while(true)
+        {
+            if(num % k == 0)
+            {
+                return len;
+            }
+
+            num = (num * 10 + 1) % k;   
+            len++;
+        }
+
+        return -1;
+    }
+};
